8ca5c9df83145052eb275031f869b6d3891697c2
Kire.Jovanoski <kire.jovanosky@gmail.com>
2023-06-23 12:23:51 +0200

    commit init -m "First commit - almost done wit jwt configuration"

---
+++ src/main/java/com/ecommerce/jwtecommerce/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.ecommerce.jwtecommerce.controller;
+
+import com.ecommerce.jwtecommerce.entity.User;
+import com.ecommerce.jwtecommerce.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostConstruct
+    public void initRolesAndUsers(){
+        userService.initRolesAndUser();
+    }
+
+    @PostMapping({"/registerNewUser"})
+    public User registerNewUser(@RequestBody User user){
+        return userService.registerNewUser(user);
+    }
+
+    @GetMapping({"/forAdmin"})
+    @PreAuthorize("hasRole('Admin')")
+    public String forAdmin(){
+        return "This URL is only accessible to administrators";
+    }
+
+    @GetMapping({"/forUser"})
+    @PreAuthorize("hasRole('User')")
+    public String forUser(){
+        return "This URL is only accessible to the users";
+    }
+
+}
---
+++ src/main/java/com/ecommerce/jwtecommerce/service/UserService.java
@@ -0,0 +1,66 @@
+package com.ecommerce.jwtecommerce.service;
+
+import com.ecommerce.jwtecommerce.dao.RoleDao;
+import com.ecommerce.jwtecommerce.dao.UserDao;
+import com.ecommerce.jwtecommerce.entity.Role;
+import com.ecommerce.jwtecommerce.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private RoleDao roleDao;
+
+    public User registerNewUser(User user){
+       return userDao.save(user);
+    }
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public void initRolesAndUser(){
+        Role adminRole = new Role();
+        adminRole.setRoleName("Admin");
+        adminRole.setRoleDescription("Admin role");
+        roleDao.save(adminRole);
+
+        Role userRole = new Role();
+        userRole.setRoleName("User");
+        userRole.setRoleDescription("Default role for newly created record");
+        roleDao.save(userRole);
+
+        User adminUser = new User();
+        adminUser.setUserFirstName("admin");
+        adminUser.setUserLastName("admin");
+        adminUser.setUserName("admin123");
+        adminUser.setUserPassword(getEncodedPassword("admin@pass"));
+        Set<Role> adminRoles = new HashSet<>();
+        adminRoles.add(adminRole);
+        adminUser.setRole(adminRoles);
+        userDao.save(adminUser);
+
+        User user = new User();
+        user.setUserFirstName("Nikola");
+        user.setUserLastName("Nikolov");
+        user.setUserName("nikola.n");
+        user.setUserPassword(getEncodedPassword("nikola@pass"));
+        Set<Role> userRoles = new HashSet<>();
+        userRoles.add(userRole);
+        user.setRole(userRoles);
+        userDao.save(user);
+
+    }
+
+    public String getEncodedPassword(String password){
+        return passwordEncoder.encode(password);
+    }
+}
