a9e7718435b919ffc790f66ecd35ccbbe7e798e0
Kire.Jovanoski <kire.jovanosky@gmail.com>
2023-07-12 13:29:44 +0200

    home page with show all products added, getAllProducts accessible without Admin role

--- src/main/java/com/ecommerce/jwt/controller/ProductController.java
+++ src/main/java/com/ecommerce/jwt/controller/ProductController.java
@@ -49,7 +49,6 @@ public class ProductController {
         return imageModels;
     }
 
-    @PreAuthorize("hasRole('Admin')")
     @GetMapping({"/getAllProducts"})
     public List<Product> getAllProducts(){
         return productService.getAllProducts();
