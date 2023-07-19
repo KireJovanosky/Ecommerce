969164851f125f18db38aeb952d34ca362b80c82
Kire.Jovanoski <kire.jovanosky@gmail.com>
2023-07-19 10:51:41 +0200

    pagination added

--- src/main/java/com/ecommerce/jwt/dao/ProductDao.java
+++ src/main/java/com/ecommerce/jwt/dao/ProductDao.java
@@ -1,9 +1,15 @@
 package com.ecommerce.jwt.dao;
 
 import com.ecommerce.jwt.entity.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductDao extends CrudRepository<Product, Integer> {
+
+    public List<Product> findAll(Pageable pageable);
+
 }
